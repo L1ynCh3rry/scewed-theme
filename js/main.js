83d6984285a1e4e9ac243d66981a05deb3590aaa
L1ynCh3rry <L1ynCh3rry@DESKTOP-F0EB124>
2017-12-16 14:39:31 +0100

    Finishing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-
+    
+    
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 150) {
@@ -89,27 +90,54 @@ $(document).ready(function () {
         });
     }
 
-    $(window).scroll(function () {
+   /* $(window).scroll(function () {
         var wh = $(window).height();
         var scroll = $(window).scrollTop();
         $('.count').each(function () {
             var position = $(this).offset().top;
-            if(position < wh + scroll - 100){
+            if (position < wh + scroll - 100) {
                 $(this).prop('Counter', 0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 4000,
-                easing: 'swing',
-                step: function (now) {
-                    $(this).text(Math.ceil(now));
-                }
-            });
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
             }
-            
-            
         });
-    });
+    }); */
 
+
+
+    var a = 0;
+    $(window).scroll(function () {
+        var oTop = $('#counter').offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+            $('.counter-value').each(function () {
+                var $this = $(this),
+                        countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+                },
+                        {
+                            duration: 4000,
+                            easing: 'swing',
+                            step: function () {
+                                $this.text(Math.floor(this.countNum));
+                            },
+                            complete: function () {
+                                $this.text(this.countNum);
+                                //alert('finished');
+                            }
+                        });
+            });
+            a = 1;
+        }
+    });
 
 
 
